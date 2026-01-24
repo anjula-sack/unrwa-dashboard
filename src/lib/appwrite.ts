97a30e3287c8cfc17220de5e28a2312ad048beb3
anjula-sack <anjulashanaka@gmail.com>
2026-01-24 12:30:21 +0530

    Fix learner section issue

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -10,7 +10,7 @@ import {
 } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "")
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
 
 export const account = new Account(client);
